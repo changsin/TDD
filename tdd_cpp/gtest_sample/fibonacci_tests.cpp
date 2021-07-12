c305e6712a3c1226ee16460331c284fa7e9387a0
Changsin <changsin@hotmail.com>
2021-07-12 23:01:46 +0900

    refactored gtest samples

---
+++ tdd_cpp/gtest_sample/fibonacci_tests.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+int fib(int id) {
+    if (id < 0) {
+        return -1;
+    }
+    if (id == 0 || id == 1) {
+        return id;
+    }
+
+    return fib(id - 2) + fib(id - 1);
+}
+
+TEST(FibTest, Valid) {
+    ASSERT_EQ(fib(-1), -1);
+    ASSERT_EQ(fib(0), 0);
+    ASSERT_EQ(fib(1), 1);
+    ASSERT_EQ(fib(3), 2);
+}
